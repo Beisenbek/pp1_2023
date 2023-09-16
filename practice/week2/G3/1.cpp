ef0569c2d9744ca2738ebf3ae4f215286b608475
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2023-09-16 18:44:31 +0600

    'upd'

---
+++ practice/week2/G3/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double a, b, c;
+    cin >> a >> b >> c;
+
+    double d = b * b - 4 * a * c;
+    
+    if(d > 0){
+        cout << (-b + sqrt(d)) / (2 * a) << endl;
+        cout << (-b - sqrt(d)) / (2 * a) << endl; 
+    }else if(d == 0){
+        cout << -b / (2 * a);
+    }
+
+    return 0;
+}
