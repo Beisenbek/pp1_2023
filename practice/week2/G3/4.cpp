9d5b874e2c907dd35f75220cf546da25a38c4bc0
Beisenbek M. Baisakov <b.baisakov@kbtu.kz>
2023-09-16 19:18:26 +0600

    'upd'

---
+++ practice/week2/G3/4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int n, k;
+    cin >> n >> k;
+
+    if(n < k){
+        cout << 2;
+    }else{
+        if((2 * n) % k > 0 ){
+            cout << (2 * n) / k + 1;
+        }else{
+            cout << (2 * n) / k;
+        }
+    }
+
+    return 0;
+}
